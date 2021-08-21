cee32c6b8db9297305ffd1fc83da9728f187665e
skanehira <sho19921005@gmail.com>
2021-08-21 21:57:32 +0900

    change example

--- example/main.go
+++ example/main.go
@@ -6,8 +6,50 @@ import (
 )
 
 func main() {
-	input := `{"hello": "world", "array": [{"name": "gorilla"}]}`
-	obj := json.NewParser(input).Parse().(map[string]interface{})
-	fmt.Println(obj["hello"])                    // world
-	fmt.Println(obj["array"].([]interface{})[0]) // map[name:gorilla]
+	input := `
+      [
+        {
+           "precision": "zip",
+           "Latitude":  37.7668,
+           "Longitude": -122.3959,
+           "Address":   "",
+           "City":      "SAN FRANCISCO",
+           "State":     "CA",
+           "Zip":       "94107",
+           "Country":   "US"
+        },
+        {
+           "precision": "zip",
+           "Latitude":  37.371991,
+           "Longitude": -122.026020,
+           "Address":   "",
+           "City":      "SUNNYVALE",
+           "State":     "CA",
+           "Zip":       "94085",
+           "Country":   "US"
+        }
+      ]
+	`
+	obj := json.NewParser(input).Parse()
+	fmt.Printf("%#v\n", obj)
+
+	input = `
+      {
+        "Image": {
+            "Width":  800,
+            "Height": 600,
+            "Title":  "View from 15th Floor",
+            "Thumbnail": {
+                "Url":    "http://www.example.com/image/481989943",
+                "Height": 125,
+                "Width":  100
+            },
+            "Animated" : false,
+            "IDs": [116, 943, 234, 38793]
+          }
+      }
+	`
+
+	obj = json.NewParser(input).Parse()
+	fmt.Printf("%#v\n", obj)
 }
